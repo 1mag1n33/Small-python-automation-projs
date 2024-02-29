d5004a4eb3a2df7dbeb47abc2a753ba2deab5dfa
1mag1n33 <kopeccarson@gmail.com>
2024-02-29 01:11:03 -0500

    commit

--- Pixel-Project/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, send_from_directory
-
-app = Flask(__name__, template_folder='./templates', static_folder="./static")
-
-@app.route('/')
-def hello_world():
-    return render_template('shh.html')
